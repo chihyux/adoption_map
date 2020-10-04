7a9866e9b7571b83e87960d539824742966c10c7
chihyux <chihyux@gmail.com>
2020-10-04 23:25:16 +0800

    change url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const urlToRequest =
   process.env.NODE_ENV === 'production'
-    ? 'https://jolly-shockley-e9aec7.netlify.app'
+    ? 'https://pets-backend-for-cors.herokuapp.com/'
     : 'http://localhost:3001'
 
 export const instance = axios.create({
